909ab18f70d395ded77abb446df5aa760ae7c213
Irvan-Hardyanto <hardyanto.irvan@gmail.com>
2025-05-22 14:06:00 +0700

    feat: add custom-made <TabLayout/> and <Button/> component

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,81 @@
+import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+const buttonClassName = cva(
+"flex items-center cursor-pointer",
+{
+  variants: {
+    variant: {
+      primary: [
+        ""
+      ],
+      secondary: [
+        ""
+      ],
+      tertiary: [
+        ""
+      ],
+    },
+    negative: {
+      true: null,
+      false: null,
+    },
+    size: {
+      default: "",
+      sm: "",
+    },
+  },
+  compoundVariants: [
+    {
+      variant: "primary",
+      negative: true,
+      class: [
+        ""
+      ],
+    },
+    {
+      variant: "secondary",
+      negative: true,
+      class: [
+        ""
+      ],
+    },
+    {
+      variant: "tertiary",
+      negative: true,
+      class: [
+        ""
+      ],
+    },
+  ],
+  defaultVariants: {
+    variant: "primary",
+    size: "default",
+  },
+}
+);
+
+type TButtonProps = {
+  className?: string;
+  negative?: boolean;
+  label: string;
+} & React.ComponentProps<"button"> & VariantProps<typeof buttonClassName>;
+
+function Button({className, variant, size, negative, label, ...props}:TButtonProps) {
+  return (
+    <button 
+     className={
+        buttonClassName({
+          variant,
+          size,
+          className,
+          negative,
+        })
+      }
+      {...props}
+            >
+              {label}
+            </button>
+  );
+}
+
+export default Button;
---
+++ src/app/components/TabLayout.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { ReactNode, useEffect, useState } from "react";
+import Button from "./Button";
+
+type TTabItem = {
+  label: string;
+  content: ReactNode;
+}
+
+type TTabLayoutProps = {
+  activeTab?: number;
+  menus: TTabItem[];
+}
+
+function TabLayout({menus}:TTabLayoutProps) {
+  const [ activeTab,setActiveTab ] = useState<number>(0);
+
+  useEffect(()=>{
+    console.log("activeTab is: ",activeTab)
+  },[activeTab])
+  return (
+    <div id="tab-container" className="flex flex-col">
+      <div id="tabs" className="flex gap-4 mb-2">
+        {menus.map((m,idx)=>{
+          return (
+            <Button
+                      key={idx}
+              data-index={idx}
+              onClick={(e)=>setActiveTab(Number(e.currentTarget.dataset.index))}
+              className="px-4 py-2 border border-amber-400 rounded-sm hover:scale-110 duration-200"
+              label={m.label}
+            />
+          )
+        })}
+      </div>
+      <div id="tab-content">
+        {
+          menus.map((m,idx)=>(
+            <div id={idx === activeTab ? "active" : "inactive" } data-index={idx} key={idx} style={{display: idx === activeTab ? "block" : "none"}} className="transition-colors">
+              {m.content}
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+export default TabLayout;
